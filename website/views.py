1100e696ae67d87c130a7587527bb5285f6a8d51
Ali Elmi <ali.elmi77@yahoo.com>
2024-02-06 21:53:40 +0330

    inheritance in template and pass data with render

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+info={"name":"Ali Elmi", "email":"ali.elmi77@yahoo.com","phone":"+989146882659"}
 def index_view(request):
-    return render(request,'index.html')
+    return render(request,'index.html', info)
 # Create your views here.
